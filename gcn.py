aca01486aa9bae9e39c8e041c9c277fc00a9c5cd
woodcutter1998 <jeric05908@gmail.com>
2023-03-14 21:29:04 -0500

    commetns

--- gcn.py
+++ gcn.py
@@ -28,6 +28,7 @@ class GCN(nn.Module):
     super(GCN, self).__init__()
     self.conv1 = dgl.nn.GraphConv(in_feats, hidden_size)
     self.conv2 = dgl.nn.GraphConv(h_feats, num_classes)
+
   def forward(self, g, in_feat):
     h = self.conv1(h, in_feat)
     h = F.relu(h)
@@ -48,7 +49,18 @@ for epoch in range(num_epochs):
         # Zero the gradients
         optimizer.zero_grad()
         # Forward pass
+        # batched_graph = [graph1, graph2], each with two nodes.
+        # break batched_graph in to batched_subgraphs   (B*N graphs) (2*2 subgraphs)
+
+        # outputs = model(batched_subgraphs)  (B*N  vectors) (4 vectors in total)
         outputs = model(batched_graph)
+
+        # [vec1, vec2, vec3, vec4]  vec1 is from node 1 of graph1, vec2 is from node 2 of graph 1
+        # vec3 is from node 1 of graph2, vec4 is from node 2 of graph 2
+
+        # full_outputs = mean\sum(outputs, axis = 1)  axis 0 is graph dimension, axis 1 is subgraph dimension
+        # full_outputs : (B vectors)
+        # full_outputs:[VEC1, VEC2] VEC1 for graph 1, VEC2 for graph2, VEC1 = vec1+ vec2, VEC2= vec3+vec4
         # Compute loss
         loss = loss_fn(outputs, labels)
         # Backward pass
